4b2df582d950f1cb7fc0729761e8676c63c672a4
sk122354 <sk122354@gmail.com>
2022-01-13 20:43:21 +0900

    Git hub uploading Test 2

---
+++ Study02/Study02/App.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study02
+{
+    class App
+    {
+        public App()
+        {
+            Console.WriteLine("Git hub upload test");
+        }
+    }
+}
